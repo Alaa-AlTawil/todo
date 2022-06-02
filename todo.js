5cead391ad9158dfbb7ac994b74a72e8a16f220f
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-06-02 23:19:57 +0300

    writing jquery that check the two text fields if they are not empty and then create a div for inserting the time to it

--- todo.js
+++ todo.js
@@ -1,2 +1,20 @@
 
- 
+     $(".add-btn").on("click",function(){
+        if($(".txtb1").val() !="" && $(".txtb2").val() !=""){
+            var d = new Date();
+            var month = d.getMonth()+1;
+            var day = d.getDate();
+            var h=d.getHours();
+            var m=d.getMinutes();
+            var s=d.getSeconds(); 
+            var output = " "+d.getFullYear() + '/' + month + '/' + day+" "+h+":"+m+":"+s;
+            var time=$("<div class='time'></div>")
+            time.append(output)
+         
+          
+          
+                   
+    
+        }
+    })
+    
